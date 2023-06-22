372b648dc91a179671b9acfac8cb86fd40cd5985
daan-ef2 <94648069+daan-ef2@users.noreply.github.com>
2023-06-22 14:10:29 +0200

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import InstantSearchWrapper from "./InstantSearchWrapper";
+
+export {RefinementTypeEnum, type FilterableAttributeProps} from "./InstantSearchWrapper";
+export default InstantSearchWrapper;
